dcee0c6794d2ad83741f767e1a50dab104071760
JananJavdan <145382151+JananJavdan@users.noreply.github.com>
2023-10-25 15:54:35 +0200

    Add files via upload

---
+++ Chapter08_Interface/src/Oefening2/Car.java
@@ -0,0 +1,13 @@
+package Oefening2;
+
+public class Car implements Switch1,Motor{
+    @Override
+    public void toggleSwitch() {
+
+    }
+
+    @Override
+    public void gas() {
+
+    }
+}
---
+++ Chapter08_Interface/src/Oefening2/Switch1.java
@@ -0,0 +1,6 @@
+package Oefening2;
+
+public interface Switch1 {
+    //maak een interface switch1
+    void toggleSwitch();
+}
