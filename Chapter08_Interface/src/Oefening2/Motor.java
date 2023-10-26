7c673efffb449eaf2101e55a19cdf630604e408a
JananJavdan <145382151+JananJavdan@users.noreply.github.com>
2023-10-26 15:32:01 +0200

    Delete Chapter08_Interface/src/Oefening2 directory

--- Chapter08_Interface/src/Oefening2/Motor.java
+++
@@ -1,6 +0,0 @@
-package Oefening2;
-
-public interface Motor {
-    //maak een interface met de naam motor
-    void gas();
-}
--- Chapter08_Interface/src/Oefening2/Switch1.java
+++
@@ -1,6 +0,0 @@
-package Oefening2;
-
-public interface Switch1 {
-    //maak een interface switch1
-    void toggleSwitch();
-}
