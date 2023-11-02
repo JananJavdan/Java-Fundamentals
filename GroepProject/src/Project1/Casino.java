f06359927f2d88db366ca82eac97ca784afcff05
JananJavdan <145382151+JananJavdan@users.noreply.github.com>
2023-11-02 09:05:53 +0100

    Add files via upload

---
+++ GroepProject/src/Project1/Casino.java
@@ -0,0 +1,16 @@
+package Project1;
+
+import java.util.Scanner;
+
+public abstract class Casino {
+    public static int playerBalance;
+    public int costPerTicket;
+
+    public Casino(int playerBalance, int costPerTicket){
+        this.playerBalance = playerBalance;
+        this.costPerTicket = costPerTicket;
+    }
+
+    public abstract void launch(Scanner scanner);
+}
+
