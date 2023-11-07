055a193dee4a9471e667f613fa802fd8862e957d
JananJavdan <145382151+JananJavdan@users.noreply.github.com>
2023-11-07 15:52:30 +0100

    Delete Chapter09_NestesClasses directory

--- Chapter09_NestesClasses/src/StaticExample/MainApp1.java
+++
@@ -1,9 +0,0 @@
-package StaticExample;
-
-public class MainApp1 {
-    public static void main(String[] args) {
-
-        OuterClass1.InnerClass innerClass = new OuterClass1.InnerClass();
-        System.out.println("Static inner X = " + innerClass.getStaticInnerX());
-    }
-}
