5ccd1f4a611446f8ad67b50f2fd83ef00360d50f
JananJavdan <145382151+JananJavdan@users.noreply.github.com>
2023-11-07 15:54:37 +0100

    Add files via upload

---
+++ Project3/DummyPhone.java
@@ -0,0 +1,18 @@
+package Project3;
+
+public class DummyPhone implements ISimCard,IRadio{
+    @Override
+    public void playChannel(double fm) {
+    }
+    @Override
+    public void changeChannel(double fm) {
+    }
+    @Override
+    public void startCall(String number) {
+        ISimCard.super.startCall(number);
+    }
+    @Override
+    public void endCall() {
+        ISimCard.super.endCall();
+    }
+}
---
+++ Project3/ISimCard.java
@@ -0,0 +1,8 @@
+package Project3;
+
+public interface ISimCard {
+    public default void startCall(String number){
+            }
+            public default void endCall(){
+            }
+}
