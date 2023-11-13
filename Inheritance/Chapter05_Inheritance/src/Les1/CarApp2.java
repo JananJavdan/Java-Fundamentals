868c5188bdfc65c5d5f50090a3fb69a975a01389
Janan Javdan <145382151+JananJavdan@users.noreply.github.com>
2023-11-13 21:22:14 +0100

    Delete Inheritance directory

--- Inheritance/Chapter05_Inheritance/src/Les1/CarApp2.java
+++
@@ -1,8 +0,0 @@
-package Les1;
-
-public class CarApp2 {
-    public static void main(String[] args) {
-
-        Car2 myCar2 = new Car2();
-    }
-}
--- Inheritance/Chapter05_Inheritance/src/Project3/ICamera.java
+++
@@ -1,7 +0,0 @@
-package Project3;
-
-public interface ICamera {
-     void shootAPhoto(double amountOfPhotos);
-     String[] viewPhotos();
-    }
-
