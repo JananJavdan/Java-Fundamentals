37da92f718baaaf495c90abeff6a438a1a0bfffe
Janan Javdan <145382151+JananJavdan@users.noreply.github.com>
2023-11-16 15:58:38 +0100

    Add files via upload

---
+++ Chapter11_Streams/src/Project2/Person.java
@@ -0,0 +1,16 @@
+package Project2;
+
+abstract class Person {
+    public void Person(){
+    }
+
+    public  Person(String String){
+    }
+
+    @Override
+    public String toString() {
+        return "Person{}";
+    }
+    String name;
+    String lastName;
+}
